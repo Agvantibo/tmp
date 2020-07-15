71b68711fc2eb87181edd9bb4cf049d3924168e9
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-15 16:51:01 +0000

    died

--- tmp.py
+++ tmp.py
@@ -3,7 +3,7 @@ array = [int(i) for i in input().split()]
 biggest = array[0]
 biggest_index = 0
 for i in range(1, len(array) - 1):
-    if i > biggest:
+    if array[i] > biggest:
         biggest_index = i
         biggest = array[i]
 print(biggest_index + 1)
