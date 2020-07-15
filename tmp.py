8b8793edbc92610a3aafa0b1f20598b9ae20cdfd
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-15 16:43:52 +0000

    clanged

--- tmp.py
+++ tmp.py
@@ -1,2 +1,9 @@
 input()
 array = [int(i) for i in input().split()]
+biggest = array[0]
+biggest_index = 0
+for i in range(1, len(array) - 1):
+    if i > biggest:
+        biggest_index = i
+        biggest = array[i]
+print(biggest_index + 1)
