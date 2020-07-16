f23efa1d8e67b45cf15b688c1ba92c9bfb475296
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-16 11:57:22 +0000

    erfref

--- tmp.py
+++ tmp.py
@@ -1,7 +1,12 @@
 input()
 array = [int(i) for i in input().split()]
-biggest = array[0]
+smallest = array[0]
 for i in array:
-    if i > biggest:
-        biggest = i
-print(array.index(biggest) + 1)
+    if i < smallest:
+        smallest = i
+array.pop(array.index(smallest))
+second_smallest = array[0]
+for i in array:
+    if i < second_smallest:
+        second_smallest = i
+print(smallest, second_smallest)
