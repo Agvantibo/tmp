41d93240fc3ae7127e53120578463aba5ee3b2dc
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-20 14:52:18 +0000

    ate a pie

--- tmp.py
+++ tmp.py
@@ -3,10 +3,10 @@ size = int(input())
 array = []
 for i in range(size):
     array.append([int(i) for i in input().split()])
-for i in array:
+for i in range(size):
     flag = False
-    for j in i:
-        if not flag and j == query:
+    for j in range(size):
+        if not flag and array[j][i] == query:
             flag = True
             print('YES')
     if not flag:
