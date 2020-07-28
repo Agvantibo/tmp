8d92f71509f77f02ae264e8c5c622e9a15d04658
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-28 15:30:59 +0000

    nuffink

--- tmp.py
+++ tmp.py
@@ -1,22 +1,19 @@
-a = []
-stdout = []
-while True:
-    c = input()
-    if c == 'exit':
-        stdout.append('bye')
-        break
-    elif c == 'last':
-        stdout.append(a[-1])
-    elif c == 'pop':
-        stdout.append(a[-1])
-        a.pop(-1)
-    elif c == 'size':
-        stdout.append(len(a))
-    elif c == 'clear':
-        a = []
-        stdout.append('ok')
-    elif 'push' in c:
-        stdout.append('ok')
-        c = c.split()
-        a.insert(0, c[1])
-print('\n'.join(map(str, stdout)))
+def binaryGet(arr, x): 
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < x: 
+            low = mid + 1
+        elif arr[mid] > x: 
+            high = mid - 1
+        else: 
+            return 'YES' 
+    return 'NO'
+input()
+array = [int(i) for i in input().split()]
+queries = [int(i) for i in input().split()]
+for i in queries:
+    print(binaryGet(array, i))
+    
