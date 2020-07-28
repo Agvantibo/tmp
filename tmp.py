bdba2f08b8931cf20ac73fd5f3e21551542b65c2
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-28 15:33:17 +0000

    rfe

--- tmp.py
+++ tmp.py
@@ -1,12 +1,12 @@
-def binaryGet(arr, x): 
+def binaryGet(arr, q): 
     low = 0
     high = len(arr) - 1
     mid = 0
     while low <= high:
         mid = (high + low) // 2
-        if arr[mid] < x: 
+        if arr[mid] < q: 
             low = mid + 1
-        elif arr[mid] > x: 
+        elif arr[mid] > q: 
             high = mid - 1
         else: 
             return 'YES' 
@@ -16,4 +16,3 @@ array = [int(i) for i in input().split()]
 queries = [int(i) for i in input().split()]
 for i in queries:
     print(binaryGet(array, i))
-    
