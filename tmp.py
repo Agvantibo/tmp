d7b9fcd633d61fb68ca4d0366fb1eb5d2e83220e
Agvantibo <dima_savchenko_96@inbox.ru>
2020-07-29 18:34:18 +0000

    sheBANGed

--- tmp.py
+++ tmp.py
@@ -1,4 +1,4 @@
-def binaryGet(arr, q): 
+def binarySearch(arr, q):
     low = 0
     high = len(arr) - 1
     mid = 0
@@ -6,13 +6,23 @@ def binaryGet(arr, q):
         mid = (high + low) // 2
         if arr[mid] < q: 
             low = mid + 1
-        elif arr[mid] > q: 
+        elif arr[mid] > q:
             high = mid - 1
         else: 
-            return 'YES' 
-    return 'NO'
+            return mid
+    return -1
 input()
 array = [int(i) for i in input().split()]
 queries = [int(i) for i in input().split()]
 for i in queries:
-    print(binaryGet(array, i))
+    testfield = array.copy()
+    rd = binarySearch(array,i)
+    print(rd + 1, end=' ')
+    testfield.pop(rd)
+    while True:
+        rd = binarySearch(array,i)
+        if rd == -1:
+            break
+        testfield.pop(rd)
+        old = rd
+print(old)
