8bd5cf8068881d32944385a150ca28cf82923a86
Agvantibo <68030524+Agvantibo@users.noreply.github.com>
2020-08-03 21:12:27 +0300

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,29 +1,42 @@
-def binarySearch(arr, q):
+input()
+arr = [int(i) for i in input().split()]
+queries = [int(i) for i in input().split()]
+
+def binaryGet(arr, x): 
     low = 0
     high = len(arr) - 1
-    mid = 0
     while low <= high:
         mid = (high + low) // 2
-        if arr[mid] < q: 
+        if arr[mid] < x: 
             low = mid + 1
-        elif arr[mid] > q:
+        elif arr[mid] > x: 
             high = mid - 1
         else: 
             return mid
     return -1
-input()
-array = [int(i) for i in input().split()]
-queries = [int(i) for i in input().split()]
 for i in queries:
-    testfield = array.copy()
-    rd = binarySearch(array,i)
-    print(rd + 1, end=' ')
-    testfield.pop(rd)
-    old = rd
-    while True:
-        rd = binarySearch(testfield,i)
-        if rd == -1:
-            break
-        testfield.pop(rd)
-        old = rd
-    print(old + 1)
+    randomElementIndex = binaryGet(arr, i)
+    lastElementIndex = randomElementIndex
+    firstElementIndex = randomElementIndex
+    try:
+        nextIndex = arr[randomElementIndex + 1]
+    except IndexError:
+        nextIndex = ''
+    try:
+        previousIndex = arr[randomElementIndex - 1]
+    except IndexError:
+        previousIndex = ''
+    if nextIndex == i:
+        for j in range(randomElementIndex, len(arr) - 1):
+            if j != i:
+                break
+            lastElementIndex += 1
+    if previousIndex == i:
+        for j in range(0, randomElementIndex, -1):
+            if j != i:
+                break
+            lastElementIndex -= 1
+    if randomElementIndex == -1:
+        print(0)
+    else:
+        print(firstElementIndex + 1, lastElementIndex + 1)
