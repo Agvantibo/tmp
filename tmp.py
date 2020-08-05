741877fdcab84b9be6c16a5066e09df11d9e0e41
Agvantibo <68030524+Agvantibo@users.noreply.github.com>
2020-08-05 18:17:37 +0000

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -1,42 +1,18 @@
-input()
-arr = [int(i) for i in input().split()]
-queries = [int(i) for i in input().split()]
+class flist():
 
-def binaryGet(arr, x): 
-    low = 0
-    high = len(arr) - 1
-    while low <= high:
-        mid = (high + low) // 2
-        if arr[mid] < x: 
-            low = mid + 1
-        elif arr[mid] > x: 
-            high = mid - 1
-        else: 
-            return mid
-    return -1
-for i in queries:
-    randomElementIndex = binaryGet(arr, i)
-    lastElementIndex = randomElementIndex
-    firstElementIndex = randomElementIndex
-    try:
-        nextIndex = arr[randomElementIndex + 1]
-    except IndexError:
-        nextIndex = ''
-    try:
-        previousIndex = arr[randomElementIndex - 1]
-    except IndexError:
-        previousIndex = ''
-    if nextIndex == i:
-        for j in range(randomElementIndex, len(arr) - 1):
-            if j != i:
-                break
-            lastElementIndex += 1
-    if previousIndex == i:
-        for j in range(0, randomElementIndex, -1):
-            if j != i:
-                break
-            lastElementIndex -= 1
-    if randomElementIndex == -1:
-        print(0)
-    else:
-        print(firstElementIndex + 1, lastElementIndex + 1)
+    def __init__(self, arr):
+        self.arr = arr
+    
+
+    def swap(self, i1, i2):
+        buffer = self.arr[i1]
+        self.arr[i1] = self.arr[i2]
+        self.arr[i2] = buffer
+
+
+input()
+array = flist([int(i) for i in input().split()])
+
+biggest_index = array.arr.index(max(array.arr))
+array.swap(0, biggest_index)
+print(' '.join(map(str, array.arr)))
